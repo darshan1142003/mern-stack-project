0c5d5276b17df5815c091ea2501037b050c57db5
Darshan Gurav <125184186+darshan1142003@users.noreply.github.com>
2024-03-22 20:22:43 -0700

    new

---
+++ js/app.js
@@ -0,0 +1,55 @@
+let button = document.getElementById("add-task");
+    let input = document.getElementById("task-name")
+    let inputValue;
+    let tasks = JSON.parse(localStorage.getItem("allTasks")) ||  []
+    let table = document.querySelector('tbody')
+  
+
+    function loadData() {
+        let storedData = JSON.parse(localStorage.getItem("allTasks"))
+        storedData.map((s) => {
+        table.insertAdjacentHTML("beforeend",`
+    <tr>
+                <td id="taskHu">${s}</td>
+                <td><button id="complete-task" onclick="completeTask(this)">Complete</button></td>
+                <td><button id="delete-task">Delete</button></td>
+            </tr>
+    `)
+    })
+}
+ loadData();
+ let Completebuttons = document.querySelectorAll(".complele-task");
+ let Completebutton = document.querySelector(".complete-task");
+
+ function addTask(){
+    inputValue = input.Value;
+    tasks.push(inputValue)
+ }
+
+ button.addEventListener("click", function (){
+    localStorage.setItem("allTasks", JSON.stringify(tasks))
+    localStorage.setItem("tasknumber", tasks.length)
+    table.insertAdjacentHTML("beforeend", `
+    <tr>
+                <td>${inputValue}</td>
+                <td><button id="complete-task" onclick="completeTask(this)">Complete</button></td>
+                <td><button id="delete-task">Delete</button></td>
+            </tr>
+    `)
+    inputValue = ""
+    input.Value = ""
+ })
+
+ function completeTask(e) {
+    e.parentElement.parentElement.querySelector('ataskHu').classList.add("completed")
+    e.disabled = true;
+ }
+
+ function deleteTask(e) {
+    if(element === e.parentElement.parentElement.querySelector('ataskHu').innerText)
+    tasks.forEach(element => {
+      tasks.splice(tasks.indexof(element),
+    });
+    localStorage.setItem('allTasks', JSON.stringify(tasks))
+    e.parentElement.parentElement.remove()
+ }
